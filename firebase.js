22e51b125b47eb5cab2fdd0f7d05cffd484a7e3f
Zafer Barış <73957984+izbari@users.noreply.github.com>
2023-05-07 23:19:08 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -3,23 +3,7 @@ import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
 
-const firebaseConfig = {
-
-  apiKey: "AIzaSyA-e91uvT1QujmKBmCNpsZcqOow_zA5kcg",
-
-  authDomain: "kuryepp.firebaseapp.com",
-
-  projectId: "kuryepp",
-
-  storageBucket: "kuryepp.appspot.com",
-
-  messagingSenderId: "506334613063",
-
-  appId: "1:506334613063:web:b22e9e5afd877fb6202b62",
-
-  measurementId: "G-3RRJBXPVP5"
-
-};
+//your config here
 
 
 
